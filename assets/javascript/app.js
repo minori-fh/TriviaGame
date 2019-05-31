08861d70041390e7bc8667a506f2cbd3bcb2f740
Minori Hashimoto <minori.fh@gmail.com>
2019-05-30 19:22:18 -0700

    js: add decrement functionality and if statements based on amount of time left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,4 +9,37 @@ $("#start").on("click", function(){
     $("#trivia-page").show();
 });
 
+//Set initial variables
+var time = 5;
+var intervalID; 
+
+//If statements based on how much time is left
+if (time > 0) { //if time is still available, perform the functions below
+    
+    function run(){
+        intervalID = setInterval(decrement, 1000)
+        $("#remaining-time").html(number)
+    }
+
+    function decrement(){ //decrement function 
+        time--; 
+        $("#remaining-time").html(time)
+
+        if (time === 0){
+            stop();
+            $("#message").html("You ran out of time!")
+        }
+    }
+
+    function stop(){ //stop function to clear out interval
+        clearInterval(intervalID); 
+    }
+
+    run();
+
+} else if (time === 0) { //if time has run out, perform the functions below 
+
+}
+
+
 }); //END document ready

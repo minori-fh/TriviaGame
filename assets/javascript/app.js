97a291de8f4e5fd29546fd1a99d3452786d0bc47
Minori Hashimoto <minori.fh@gmail.com>
2019-05-31 18:05:37 -0700

    add empty div for correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 //Document ready
 $(document).ready(function() {
 
-$("#trivia-page").hide();
+// $("#trivia-page").hide();
 
 //On click functionality to start trivia game
 $("#start").on("click", function(){
     $("#first-page").hide();
     $("#trivia-page").show();
-    startClock();
+    // startClock();
 });
 
 //Create array of objects for trivia game questions and answers
@@ -52,17 +52,17 @@ function startClock(){
     time = 10; 
     console.log("working")
     intervalID = setInterval(decrement, 1000)
-    $("#remaining-time").html(time)
+    $("#remaining-time").html("Time remaining: " + time)
 }
 //function: decrement
 function decrement(){ 
     if (time > 0){
         time--; 
-        $("#remaining-time").html(time)
+        $("#remaining-time").html("Time remaining: " + time)
     
         if (time === 0){
             stop();
-            $("#message").html("You ran out of time!")
+            $("#dwight-says").html("You ran out of time!")
         }
     }
 }
@@ -74,11 +74,13 @@ function stopClock(){
 //function: to go through list of questions
 function newQuestion(){
     if (questionIndex >= 0 && questionIndex <=5){
+        $("input[name=rad]").attr("checked",false) 
         questionIndex ++
         startClock();
 
         $("dwight-says").html("Question")
         $("#win-lose").html("")
+        $("#correct-answer").html("")
 
         $("#question").html(triviaGame[questionIndex].question)
         $("#0").val(triviaGame[questionIndex].answer[0])
@@ -90,7 +92,7 @@ function newQuestion(){
 
 //function: clear out radio buttons
 function clearRadio(){
-    $("input[type=radio]").attr("checked",false) //clear out radio buttons
+    $("input[name=rad]").attr("checked",false) //clear out radio buttons
 }
 
 
@@ -110,112 +112,39 @@ $(".input-group-text").click(function(){
         if (id === triviaGame[questionIndex].correct){ //if the id of the input that was clicked on matches the correct value 
             correctCount++;
             answered = true; 
-            $("#win-lose").html("Correct!")
             $("#dwight-says").html("'FACT'")
             $("#site-left").css("background-color", "rgb(116, 187, 128)"); 
             stopClock(); //stop clock
             setTimeout(newQuestion, 5000) //delay nextQuestion 
-            setTimeout(clearRadio,5000)
 
         } else if (id != triviaGame[questionIndex].correct){ //if the id of the input that was clicked on does not match the correct value 
             incorrectCount++; 
             answered = true; 
-            $("#win-lose").html("Incorrect!")
             $("#dwight-says").html("'FALSE'")
+            $("#correct-answer").html("The correct answer is: " + triviaGame[questionIndex].correct)
             $("#site-left").css("background-color", "rgb(153, 28, 28)");
             stopClock(); //stop clock 
             setTimeout(newQuestion, 5000) //delay nextQuestion 
-            setTimeout(clearRadio,5000)
         } else { //if the player does not pick an answer
-            $("#win-lose").html("You have run out of time!")
+            $("#dwight-says").html("You have run out of time!")
+            $("#correct-answer").html("The correct answer is: " + triviaGame[questionIndex].correct)
             if (time === 0){
                 stopClock();
                 setTimeout(newQuestion, 5000) //delay nextQuestion 
-                setTimeout(clearRadio,5000)
             }
         }
     //if TIME HAS RUN OUT
     } else if (time === 0){
         stopClock();
         if (answered === false){
-            $("#message").html("You ran out of time!")
+            $("#dwight-says").html("You ran out of time!")
+            $("#correct-answer").html("The correct answer is: " + triviaGame[questionIndex].correct)
             setTimeout(newQuestion, 5000) //delay nextQuestion 
-            setTimeout(clearRadio,5000)
         } else if (answered){
             setTimeout(newQuestion, 5000) //delay nextQuestion 
-            setTimeout(clearRadio,5000)
         }
     }
     });
 //NEW-END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //If statements based on how much time is left
-// if (time > 0) { //if time is still available, perform the functions below
-
-//     //on-click functionality for when a player picks an answer 
-//     $(".input-group-text").click(function(){
-//         answered = true; 
-//         id = $(this).attr("id");
-//         if (id === triviaGame[questionIndex].correct){ //if the id of the input that was clicked on matches the correct value 
-//             correctCount++;
-//             $("#win-lose").html("Correct!")
-//             $("#dwight-says").html("FACT")
-//             $("#site-left").css("background-color", "rgb(116, 187, 128)"); 
-//         } else { //if the id of the input that was clicked on does not match the correct value 
-//             incorrectCount++; 
-//             $("#win-lose").html("Incorrect!")
-//             $("#dwight-says").html("FALSE")
-//             $("#site-left").css("background-color", "rgb(153, 28, 28)");
-//         }
-//     })
-
-//     function decrement(){ //decrement function 
-//         time--; 
-//         console.log(time)
-//         $("#remaining-time").html(time)
-
-//         if (time === 0){
-//             stop();
-//             $("#message").html("You ran out of time!")
-//         }
-//     }
-
-//     function stopClock(){ //stop function to clear out interval
-//         clearInterval(intervalID); 
-//     }
-//     //if statements based on whether the question has been answered or not
-//     if (answered = false){ 
-//     } else if (answered){ //else if the question has been answered
-//         function stop(){ //stop function to clear out interval
-//             clearInterval(intervalID); 
-//         }
-//     }
-
-
-// } else if (time === 0) { //if time has run out, perform the functions below 
-//     stopClock();
-//     $("#message").html("You ran out of time!")
-// }
 
 }); //END document ready
